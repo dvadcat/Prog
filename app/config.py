d3dd96f5bbc54ae5af0f7d2d4915b50f19e7e1c4
dvadcat <83099623+dvadcat@users.noreply.github.com>
2026-01-11 16:11:26 +0300

    Delete app directory

--- app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///risk_management.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-for-risk-management-app'
